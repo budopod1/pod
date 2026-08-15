851517f6b9f4acc3006091fcef6d1adf31d04fda
Martin Staab <martin.e.staab@gmail.com>
2026-08-15 12:33:09 -0700

    Added: `getenv`, `setenv`, `unsetenv` builtins | Moved: io related builtins to new builtins/io.epsl | Fixed: inputline.c uses proper allocation functions

--- inputline.c
+++ inputline.c
@@ -9,19 +9,20 @@
 #include <readline/history.h>
 
 ARRAY_char *IL_inputline(ARRAY_char *prompt) {
-    char *c_prompt = malloc(prompt->length + 1);
+    char *c_prompt = epsl_malloc(prompt->length + 1);
     memcpy(c_prompt, prompt->content, prompt->length);
     c_prompt[prompt->length] = '\0';
     
     char *line = readline(c_prompt);
+    free(c_prompt);
+
     if (line && *line) {
         add_history(line);
     }
 
-    if (!line) line = calloc(1, 1);
+    if (!line) line = epsl_calloc(1, 1);
 
-    free(c_prompt);
-    struct ARRAY_char *result = malloc(sizeof(*result));
+    struct ARRAY_char *result = epsl_malloc(sizeof(*result));
     result->ref_counter = 0;
     result->length = strlen(line);
     result->capacity = result->length + 1;
