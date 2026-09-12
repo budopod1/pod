ec1c0df1aa6c9e8c15284bca3c5105b31a7d58a7
Martin Staab <martin.e.staab@gmail.com>
2026-09-11 21:24:06 -0700

    Updated: install.py

--- install.py
+++ install.py
@@ -1,14 +1,29 @@
 from pathlib import Path
+import os
 
 
 BIN_FOLDER = Path(__file__).parent / "bin"
 
 
-def install():
-    add_in_script_path = Path.home() / ".profile"
+def get_user_shell_name() -> str | None:
+    if "SHELL" in os.environ:
+        return Path(os.environ["SHELL"]).parts[-1]
+    return None
+
+
+def add_to_file(script_path: Path):
+    if not script_path.exists():
+        return
     path_to_add = Path(BIN_FOLDER).absolute()
-    with open(add_in_script_path, "a") as file:
+    with open(script_path, "a") as file:
         file.write(f"\nexport PATH=$PATH:{path_to_add}\n")
+
+
+def install():
+    add_to_file(Path.home() / ".profile")
+
+    shell = get_user_shell_name() or "bash"
+    add_to_file(Path.home() / f".{shell}rc")
 
 
 if __name__ == "__main__":
